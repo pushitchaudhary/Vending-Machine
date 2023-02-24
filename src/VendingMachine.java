793d1380e0575a76175b5ef387b56010f9b9141f
pushitchaudhary <pushitcoc@gmail.com>
2023-02-24 17:10:17 +0545

    Vending Machine

---
+++ src/VendingMachine.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class VendingMachine {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JRadioButton radioButton1;
+    private JRadioButton radioButton2;
+    private JRadioButton radioButton3;
+    private JRadioButton radioButton4;
+    private JRadioButton radioButton5;
+    private JRadioButton radioButton6;
+    private JButton button1;
+    private JButton button2;
+    private JButton vButton;
+    private JPanel main;
+}
