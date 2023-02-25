1234659e7a42b1406a6bcf805b756f226ad1a7db
pushitchaudhary <pushitcoc@gmail.com>
2023-02-25 20:56:24 +0545

    second commit

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,16 +1,35 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class VendingMachine {
     private JTextField textField1;
     private JTextField textField2;
-    private JRadioButton radioButton1;
-    private JRadioButton radioButton2;
-    private JRadioButton radioButton3;
-    private JRadioButton radioButton4;
-    private JRadioButton radioButton5;
-    private JRadioButton radioButton6;
-    private JButton button1;
-    private JButton button2;
-    private JButton vButton;
+    private JRadioButton spriteNRP8000RadioButton;
+    private JRadioButton fantaNRP5000RadioButton;
+    private JRadioButton mountainDewNRP70RadioButton;
+    private JRadioButton sliceNRP4000RadioButton;
+    private JRadioButton redBullNRP120RadioButton;
+    private JRadioButton cokeNRP7000RadioButton;
+    private JButton purchaseButton;
+    private JButton clearButton;
+    private JButton exitButton;
     private JPanel main;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("VendingMachine");
+        frame.setContentPane(new VendingMachine().main);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public VendingMachine() {
+    purchaseButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    });
+}
 }
